0eacfab56671144ad77a2dacbe849d16ac117b36
Jordan Moreira <jordanmoreira231@gmail.com>
2022-02-21 22:23:58 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const previous=document.querySelector("[previous-operand]").value
+const current=document.querySelector("[current-operand]").value
+const clear=document.querySelector("[clear]")
+const deletecurrent=document.querySelector(".button-delete")
+const number=document.querySelectorAll("[number]")
+const operand=document.querySelectorAll("[operand]")
+const equals=document.querySelector("[equals]").value
+
